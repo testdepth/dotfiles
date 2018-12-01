b1e100742b0d6069089e4c0e21b0b03c12225366
Nehil Jain <nehiljain@Nehils-MacBook-Air.local>
2018-11-30 23:51:34 -0500

    added personal stuff to brew.sh

--- brew.sh
+++ brew.sh
@@ -21,7 +21,7 @@ brew cask install iterm2
 
 
 # Google Chrome
-brew cask install google-chrome
+# brew cask install google-chrome
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -62,31 +62,6 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -94,23 +69,12 @@ brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install speedtest_cli
 brew install ssh-copy-id
 brew install testssl
 brew install tree
-brew install vbindiff
-brew install webkit2png
 brew install zopfli
 brew install ngrok
 
-# Install useful Frontend tools
-brew install node
 
 # Install useful languages
 brew cask install java 
@@ -122,14 +86,12 @@ brew tap homebrew/science
 brew install R
 brew install Caskroom/cask/rstudio
 
-# Install Infrastructure toolchains
-brew install heroku-toolbelt
 
 brew cask install keepingyouawake
 brew cask install flux
+brew install fish
 
 # Install Databases
-brew install mongo
 brew install postgresql
 
 # Remove outdated versions from the cellar.
