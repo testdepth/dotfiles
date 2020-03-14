d6b3c81652276dda23d8a5ebffe5ec755d10de9a
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2020-03-14 16:33:12 -0400

    Update brew.sh
    Running it while setting up new laptop and making sure that all the brew commands are correct and required for my use

--- brew.sh
+++ brew.sh
@@ -52,7 +52,7 @@ fi;
 brew install gpg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen
@@ -66,47 +66,21 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
 brew install bfg
 brew install tree
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install john
-brew install knock
-brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
 brew install xz
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install gs
 brew install imagemagick --with-webp
-brew install lynx
 brew install pigz
 brew install pv
 brew install rename
 brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
-brew install ngrok
+brew cask install ngrok
 
 # Install useful Frontend tools
 brew install node
@@ -118,11 +92,9 @@ brew install pyenv
 brew install pyenv-virtualenv
 
 # Install R and RStudio
-brew tap homebrew/science
 brew install R
 brew install Caskroom/cask/rstudio
 
-brew install dbt
 brew install wget
 
 
