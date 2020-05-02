f6eacc755ed1ac1228624924de3322cabf41638e
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2020-05-02 15:21:21 -0400

    Create git_current_branch.fish

---
+++ fish/functions/git_current_branch.fish
@@ -0,0 +1,5 @@
+function git_current_branch
+    set ref (git symbolic-ref HEAD 2> /dev/null); or \
+    set ref (git rev-parse --short HEAD 2> /dev/null); or return
+    echo $ref | sed -e 's|^refs/heads/||'
+end
