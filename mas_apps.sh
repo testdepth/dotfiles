9cbb0b8bb02ba4f40c2da32d79ff9b8ba7fa8a69
Nehil Jain <jain.nehil@gmail.com>
2022-05-08 09:00:13 -0400

    Todoist to apps

--- mas_apps.sh
+++ mas_apps.sh
@@ -9,5 +9,7 @@ mas install 1176895641
 mas install 1278508951
 # Pocket Read it later
 mas install  568494494
+# Todoist
+mas install 585829637
 # Install notes cli app https://github.com/pimterry/notes
 curl -L https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
